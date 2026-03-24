f0be6c567ad750f4c7e9c203a84f03645fcef0bd
Athlaes <chartierlucas@outlook.fr>
2026-03-24 23:58:29 +0100

    fix deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,12 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideAnimationsAsync(),
     provideHttpClient(withFetch()),
     provideRouter(routes),
     provideZonelessChangeDetection()
